b718e360cf8bda93a0a353db200d335e5226a784
AshwinSH2000 <33159171+AshwinSH2000@users.noreply.github.com>
2025-10-27 23:54:24 +0530

    Time: 6 ms (85.74%) | Memory: 18 MB (68.37%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,23 +1,30 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        newstr1 = ""
-        newstr2 = ""
-        i, j =0, len(s)-1
+        news = ""
+        for i in s:
+            if i.isalnum():
+                news+=i.lower()
+            
+        return news == news[::-1]
+        # version 1
+        # newstr1 = ""
+        # newstr2 = ""
+        # i, j =0, len(s)-1
 
-        while(i != len(s) and j != -1):
-            if s[i].isalnum() == True:
-                newstr1 += s[i].lower()
-            if s[j].isalnum() == True:
-                newstr2 += s[j].lower()
-            i+=1 
-            j-=1
-        print(newstr1)
-        print(newstr2)
-        if newstr1 == newstr2:
-            return True
-        else:
-            return False
-
+        # while(i != len(s) and j != -1):
+        #     if s[i].isalnum() == True:
+        #         newstr1 += s[i].lower()
+        #     if s[j].isalnum() == True:
+        #         newstr2 += s[j].lower()
+        #     i+=1 
+        #     j-=1
+        # print(newstr1)
+        # print(newstr2)
+        # if newstr1 == newstr2:
+        #     return True
+        # else:
+        #     return False
+        # version 1
 
 
         
