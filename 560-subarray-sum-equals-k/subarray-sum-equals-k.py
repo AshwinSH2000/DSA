d259ba6af412c33d5475a5467327353943892b7a
AshwinSH2000 <33159171+AshwinSH2000@users.noreply.github.com>
2026-01-07 01:13:04 +0530

    Time: 20 ms (97.72%) | Memory: 22 MB (12.01%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -0,0 +1,26 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        '''
+        use a hashmap to store all the previous prefix sums
+        then check if key current_sum - k exists in the hashmap
+        increment the count with its value
+        '''
+        prevSumMap = {0: 1}
+        runningSum = 0
+        count = 0
+        for i in nums:
+            runningSum += i
+            if (runningSum - k) in prevSumMap:
+                count += prevSumMap[runningSum - k]
+                if runningSum in prevSumMap:
+                    prevSumMap[runningSum] += 1
+                else:
+                    prevSumMap[runningSum] = 1
+            else:
+                if runningSum in prevSumMap:
+                    prevSumMap[runningSum] += 1
+                else:
+                    prevSumMap[runningSum] = 1
+
+        return count
+        
