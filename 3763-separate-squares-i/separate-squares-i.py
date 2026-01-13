7e00ddc4eee0bef67179ce797981cd087cc81854
AshwinSH2000 <33159171+AshwinSH2000@users.noreply.github.com>
2026-01-14 00:20:45 +0530

    Time: 2000 ms (58.94%) | Memory: 48.4 MB (19.65%) - LeetSync

--- 3763-separate-squares-i/separate-squares-i.py
+++ 3763-separate-squares-i/separate-squares-i.py
@@ -13,39 +13,40 @@ class Solution:
         def area(midY):
             total_area = 0
             for square in squares:
-                if square[1]+square[2] <= midY:
-                    total_area += square[2]*square[2]
-                elif square[1] <= midY and square[1]+square[2] > midY:
-                    total_area += square[2]* (midY-square[1])
-                # else: #square[1] > midY:
-                    # total_area += 0 #because it is above the midY
-                
+                if square[1] + square[2] <= midY:
+                    total_area += square[2] * square[2]
+                elif square[1] <= midY and square[1] + square[2] > midY:
+                    total_area += square[2] * (midY - square[1])
+
             return total_area
 
         TOTAL = 0
         minY, maxY = float("inf"), 0
         midY = 0
         for square in squares:
-            TOTAL += (square[2]*square[2])
-            if square[1]<minY:
+            TOTAL += square[2] * square[2]
+            if square[1] < minY:
                 minY = square[1]
-            if (square[1]+square[2])>maxY:
-                maxY = (square[1]+square[2])
-            
-        HALF_TOTAL = TOTAL/2
-        
-        while(maxY-minY>=0.00001):
-            midY = (maxY+minY)/2
+            if (square[1] + square[2]) > maxY:
+                maxY = square[1] + square[2]
+
+        HALF_TOTAL = TOTAL / 2
+
+        while maxY - minY >= 0.00001:
+            midY = (maxY + minY) / 2
             cur_area = area(midY)
-            if cur_area >= HALF_TOTAL:
+            if cur_area >= HALF_TOTAL:  # why is it >= ... think
                 maxY = midY
-            elif cur_area < HALF_TOTAL:
+            else:
                 minY = midY
-            
+
         return midY
 
-        
-
-
-
-        
+        """
+        its >= in line 38 because, even if the cur_area == HALF_TOTAL, 
+        we want to bring the midY line down. 
+        Yes, when the two areas are equal, midY is exactly (minY+maxY)/2 but 
+        remember in the first example, it is shown very well. 
+        the exact midY is 1.5 but we need to choose the min value of all possible y. 
+        so the minimum value is 1. to help with this calculation, we choose >= 
+        """
