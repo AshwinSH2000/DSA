a737b55f75a34ab6d7eba405a9d0f90e2ef70113
AshwinSH2000 <33159171+AshwinSH2000@users.noreply.github.com>
2026-01-14 07:28:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (7.96%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -1,6 +1,14 @@
 import math
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
+        '''
+        1. deduced that the formula is nCr where n is the rowIndex and r is the index i
+        2. for O(rowIndex) space complexity, if you deduce the formula, then there is no need to 
+            come calculating from the top. so you can simply define a 1D list of 1s and only change values
+            from index 1 to rowIndex-1
+
+        '''
+
         triangle = [1] * (rowIndex+1)
         if rowIndex < 2:
             return triangle
@@ -8,3 +16,4 @@ class Solution:
         for i in range(1,rowIndex):
             triangle[i] = math.comb(rowIndex,i)
         return triangle
+
