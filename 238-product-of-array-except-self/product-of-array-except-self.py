422bc781152c7f9d2f2f1b4b20c7b771c39337aa
AshwinSH2000 <33159171+AshwinSH2000@users.noreply.github.com>
2026-01-15 16:44:48 +0530

    Time: 2638 ms (5.48%) | Memory: 28 MB (5.33%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -0,0 +1,35 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        '''
+        logic behind this
+        have two independent loops and find the prefix and suffix products: O(n)
+        in the third iteration, to find the product of array w/o num, multiply its
+        prefix sum to from its left side and its suffix sum from its right side
+        '''
+        prefixSum = []
+        suffixSum = []
+        n = len(nums)
+        for i in range(n):
+            if not i:
+                prefixSum.append(nums[i])
+            else:
+                prefixSum.append(nums[i]*prefixSum[i-1])
+
+        for i in range(n-1, -1, -1):
+            if i==n-1:
+                suffixSum.append(nums[i])
+            else:
+                # x=
+                suffixSum.insert(0,nums[i]*suffixSum[0])
+
+        # ok now need to find the product of left and right
+        result = []
+        for i in range(n):
+            if i==0:
+                result.append(suffixSum[i+1])
+            elif i==n-1:
+                result.append(prefixSum[i-1])
+            else:
+                result.append(prefixSum[i-1]*suffixSum[i+1])
+        
+        return result
