ffbd43a8867d792729814e9488059d0581160162
AshwinSH2000 <33159171+AshwinSH2000@users.noreply.github.com>
2026-01-25 10:57:08 +0000

    Time: 3 ms (97.60%) | Memory: 19.5 MB (15.85%) - LeetSync

--- 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -2,6 +2,18 @@ class Solution:
     def minimumDifference(self, nums: List[int], k: int) -> int:
         '''
         sort the array and apply sliding window pattern
+        since you have already sorted, the min and max will always be the first 
+        element of the window and the last element of the window respectively.
+        so you only need to find its difference and compare it with old difference.
+        simply shortened the code, lol. check below comment for better understanding.
+        '''
+
+        nums.sort()
+        minDiff = nums[k-1]-nums[0]
+        for i in range(1, len(nums)-k+1):
+            minDiff = min(minDiff, nums[i+(k-1)]-nums[i])
+        return minDiff
+
         '''
         if k==1:
             return 0
@@ -18,7 +30,7 @@ class Solution:
             minDiff = min(minDiff, Max-Min)
 
         return minDiff
-
+        '''
         
         
         
