b9d930040874d939de0f0451bb283b6d709374ef
AshwinSH2000 <33159171+AshwinSH2000@users.noreply.github.com>
2026-02-06 19:25:45 +0000

    Time: 371 ms (5.05%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,8 +1,8 @@
 -- Write your PostgreSQL query statement below
 SELECT d.name as Department, e.name as Employee, e.salary as Salary
-FROM Employee e, Department d 
-WHERE e.departmentId = d.id
-AND (e.departmentId, e.Salary) IN (
+FROM Employee e JOIN Department d 
+ON e.departmentId = d.id
+WHERE (e.departmentId, e.Salary) IN (
     SELECT employee.departmentId, MAX(employee.salary)
     FROM Employee
     GROUP BY employee.departmentId
