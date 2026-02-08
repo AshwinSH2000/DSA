0f96d88ac4b012fdb68792642cfde1b94251aa71
AshwinSH2000 <33159171+AshwinSH2000@users.noreply.github.com>
2026-02-08 19:08:06 +0000

    Time: 40 ms (46.77%) | Memory: 45 MB (37.94%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -6,23 +6,41 @@
 #         self.right = right
 '''
 i just solved max depth. isnt this very similar to that?
-replace max with min
+replace max with min... that leads to a small problem
+1st approach was to counter that (currently commented). 
+2nd approach is slightly better. (to avoid float(inf))
+3rd approach is BFS which is the fastest
 '''
+# Approach 2
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
         if root is None:
             return 0
         
-        leftDepth = self.minDepth(root.left)
-        rightDepth = self.minDepth(root.right)
+        if not root.left:
+            return 1+self.minDepth(root.right)
+        
+        if not root.right:
+            return 1+self.minDepth(root.left)
+        
+        return 1+min(self.minDepth(root.left), self.minDepth(root.right))
 
-        if leftDepth == 0 and rightDepth == 0:
-            return 1
-        if leftDepth == 0:
-            leftDepth = float(inf)
-        elif rightDepth == 0:
-            rightDepth = float(inf)
+# Approach 1
+# class Solution:
+#     def minDepth(self, root: Optional[TreeNode]) -> int:
+#         if root is None:
+#             return 0
+        
+#         leftDepth = self.minDepth(root.left)
+#         rightDepth = self.minDepth(root.right)
 
-        return 1+min(leftDepth, rightDepth)
+#         if leftDepth == 0 and rightDepth == 0:
+#             return 1
+#         if leftDepth == 0:
+#             leftDepth = float(inf)
+#         elif rightDepth == 0:
+#             rightDepth = float(inf)
+
+#         return 1+min(leftDepth, rightDepth)
         
         
