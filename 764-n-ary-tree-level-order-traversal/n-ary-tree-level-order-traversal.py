05e98fea2c09121c77a5d2072a385ebeac76b40a
AshwinSH2000 <33159171+AshwinSH2000@users.noreply.github.com>
2026-02-11 18:45:22 +0000

    Time: 52 ms (57.94%) | Memory: 21.1 MB (46.79%) - LeetSync

---
+++ 764-n-ary-tree-level-order-traversal/n-ary-tree-level-order-traversal.py
@@ -0,0 +1,35 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+'''
+simple approach as the binary level order traversal. 
+only thing to keep in mind is to check if the children exist and if they do, push them into the queue
+'''
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if not root:
+            return []
+        q = []
+        q.append(root)
+        result = []
+        while q:
+            level_nodes = []
+            level_nodes_len = len(q)
+            while level_nodes_len > 0:
+                node = q.pop(0)
+                level_nodes.append(node.val)
+                level_nodes_len -= 1
+
+                if node.children:
+                    for child in node.children:
+                        q.append(child)
+
+            result.append(level_nodes)
+
+        return result
+        
